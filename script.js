6046d782b89c073a96d56e8f384b20ebf7d68bb4
RIDEMBIACOLO <ridemlucero@gmail.com>
2024-09-03 01:29:20 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the default form submission
+    alert('Form submitted!'); // Alert the user
+});
